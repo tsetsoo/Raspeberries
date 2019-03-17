9cbaba7eb03b938c5b8230326059e0d5781b351c
tsetsoo <tsvetelin.pantev@gmail.com>
2019-03-17 08:00:53 +0200

    add recognition by new photo

--- FacialRecognition/test.py
+++ FacialRecognition/test.py
@@ -5,10 +5,11 @@ import time
 import cv2
 import os
 
- to build a custom face recognition datasetPython
+realLabels = {0: "simo", 1: "tsetso"}
+numberLabels = {"simo": 0, "tsetso": 1}
 
 # load OpenCV's Haar cascade for face detection from disk
-detector = cv2.CascadeClassifier(args["cascade"])
+detector = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 # initialize the video stream, allow the camera sensor to warm up,
 # and initialize the total number of example faces written to disk
@@ -26,7 +27,13 @@ rects = detector.detectMultiScale(
 cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
              
                 # loop over the face detections and draw them on the frame
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read("trained.xml")
 for (x, y, w, h) in rects:
     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+	index_predicted, config = face_recognizer.predict(frame[y:y+h, x:x+w])
+	person_predicted = realLabels[index_predicted]
+	cv2.putText(frame, 'Prediction: ' + person_predicted, (10,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 6)
+    cv2.imshow("Recognizing face", frame)
+
 
